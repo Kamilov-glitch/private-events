793d11fb5de1ad8b16b8faf170a56e78364d16b5
Fuad <kamilovfuad@gmail.com>
2021-12-15 13:07:08 +0400

    Add a users controller, users index view and a route to that view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -64,6 +64,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:name, :date, :place)
+      params.require(:event).permit(:name, :date, :place, :creator_id)
     end
 end
