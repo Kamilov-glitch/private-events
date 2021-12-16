c15d68d3828c45c851e030e7cbae971dc90d98ef
Fuad <kamilovfuad@gmail.com>
2021-12-16 21:56:59 +0400

    Add a way to show the event user joined rather than root_path, also show join button only to signed in user

--- app/controllers/event_participatings_controller.rb
+++ app/controllers/event_participatings_controller.rb
@@ -9,18 +9,17 @@ class EventParticipatingsController < ApplicationController
     end
 
     def create
-      # @event = Event.find(params[:event_id])
       @event_participating = current_user.event_participatings.build(event_participatings_params)
-  
+      @event = Event.find(event_participatings_params[:event_id])
       respond_to do |format|
         if @event_participating.save
-          format.html { redirect_to root_path, notice: "You successfully joined an event!" } #event < root_path
+          format.html { redirect_to @event, notice: "You successfully joined an event!" } #event < root_path
           format.json { render :show, status: :created, location: root_path } #@event
         else
           flash.now[:warning] = 'There were problems when trying to create a new event registration'
 
           format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @event.errors, status: :unprocessable_entity }
+          format.json { render json: @event_participating.errors, status: :unprocessable_entity }
         end
       end
     end
