e3df8a129a7f3d7162542e85b855aaca619347bf
Fuad <kamilovfuad@gmail.com>
2021-12-18 13:18:21 +0400

    Add past_events and upcoming_events class method to Event model and to events index view

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,13 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
     has_many :event_participatings
     has_many :participants, through: :event_participatings, source: :user
-    
+
+    def self.past_events
+      self.where("date <= ?", Time.now)
+    end
+
+    def self.upcoming_events
+      self.where("date > ?", Time.now)
+    end
+
 end
